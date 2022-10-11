135dbefa1468c584912fc97ee69aa16d3828b576
Tal Oplatka <taloplatka@gmail.com>
2022-10-11 15:42:25 +0300

    display answer

--- js/solver.js
+++ js/solver.js
@@ -1,28 +1,29 @@
-equation = {
-    'variablesQuantity': 0,
-    'solution': 0
-}
-
 const variablesQuantityInput = document.getElementById('variablesQuantity');
-const variablesQuantityAlert = document.getElementById('variablesQuantityAlert');
+const validationAlert = document.getElementById('variablesQuantityAlert');
 const solutionInput = document.getElementById('solution');
-const variablesQuantitySubmitButton = document.getElementById('variablesQuantitySubmitButton');
-const variablesValuesInputs = document.getElementById('variablesValuesInputs');
+const okButton = document.getElementById('variablesQuantitySubmitButton');
+const variablesValuesInputsForm = document.getElementById('variablesValuesInputs');
 
-variablesQuantitySubmitButton.addEventListener('click', () => {
-    variablesValuesInputs.innerHTML = ''
+let variableInputs = [];
+let variablesPossibilities = []
+
+
+
+okButton.addEventListener('click', () => {
+    variableInputs = [];
+    variablesValuesInputsForm.innerHTML = '';
+    validationAlert.innerText = '';
 
     const variablesQuantity = parseInt(variablesQuantityInput.value);
     const solution = parseInt(solutionInput.value);
 
-    if (variablesQuantity <= 0) {
-        variablesQuantityAlert.style.display = 'block';
+    if (!isVariablesQuantityValid(variablesQuantity)) {
+        getVariablesQuantityValidationNotifications(variablesQuantity).forEach((notification) => {
+            validationAlert.innerText += ' ' + notification;
+        } )
     }
     else {
-        variablesQuantityAlert.style.display = 'none';
-        
-        equation['variablesQuantity'] = variablesQuantity;
-        equation['solution'] = solution;
+        validationAlert.innerText = '';
 
         for (let i = 1; i <= variablesQuantity; i++) {
             const container = document.createElement('div');
@@ -32,22 +33,29 @@ variablesQuantitySubmitButton.addEventListener('click', () => {
             const variablesValuesInput = document.createElement('input');
             variablesValuesInput.type = 'text';
             variablesValuesInput.placeholder = 'Possible Values';
+            variableInputs.push(variablesValuesInput);
 
 
             container.appendChild(variableDisplay);
             container.appendChild(variablesValuesInput);
-            variablesValuesInputs.appendChild(container);
+            variablesValuesInputsForm.appendChild(container);
             
         };
 
         const getAnswerButton = document.createElement('input');
-        getAnswerButton.type = 'button';   
+        getAnswerButton.type = 'button';
         getAnswerButton.value = 'Get Answer';
         getAnswerButton.classList.add('get-answer-button');
-        variablesValuesInputs.appendChild(getAnswerButton)
+        variablesValuesInputsForm.appendChild(getAnswerButton);
         getAnswerButton.addEventListener('click', () => {
-            alert('OK')
+            variablesPossibilities = [];
+            variableInputs.forEach((input) => {
+                variablesPossibilities.push(getPossibleVariableValuesFromInput(input.value));
+            })
+            Swal.fire('The Answer Is ' + getNumberOfSolutions(variablesPossibilities, solution));  
         })
+
     };
 
 });
+
